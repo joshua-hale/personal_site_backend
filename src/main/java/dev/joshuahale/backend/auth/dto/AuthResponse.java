2c4288d3154ec6fef5e0c53b019206d3b5d25cc1
Joshua Hale <joshuahale173@gmail.com>
2025-09-30 13:57:50 +0100

    feat(auth): add AuthResponse, LoginRequest and SignupRequest DTO's

--- src/main/java/dev/joshuahale/backend/auth/dto/AuthResponse.java
+++ src/main/java/dev/joshuahale/backend/auth/dto/AuthResponse.java
@@ -1,4 +1,51 @@
 package dev.joshuahale.backend.auth.dto;
 
+
+import java.util.Set;
+
 public class AuthResponse {
+
+    private Long userId;
+    private String username;
+    private String email;
+    private Set<String> roles;
+
+    public AuthResponse(Long userId, String username, String email, Set<String> roles) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
 }
--- src/main/java/dev/joshuahale/backend/auth/dto/SignupRequest.java
+++ src/main/java/dev/joshuahale/backend/auth/dto/SignupRequest.java
@@ -1,4 +1,44 @@
 package dev.joshuahale.backend.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class SignupRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String username;
+
+    @NotBlank
+    @Size(min = 8, max = 255)
+    private String password; // plain text, will be hashed in service
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
