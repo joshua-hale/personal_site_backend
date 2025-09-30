c86645b95df602d57f9b0722ff76e05e899a7a9a
Joshua Hale <joshuahale173@gmail.com>
2025-09-30 13:57:50 +0100

    feat(auth): add AuthResponse, LoginRequest and SignupRequest DTO's

--- src/main/java/dev/joshuahale/backend/auth/dto/LoginRequest.java
+++ src/main/java/dev/joshuahale/backend/auth/dto/LoginRequest.java
@@ -1,4 +1,28 @@
 package dev.joshuahale.backend.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequest {
+
+    @NotBlank
+    private String emailOrUsername;
+
+    @NotBlank
+    private String password;
+
+    public String getEmailOrUsername() {
+        return emailOrUsername;
+    }
+
+    public void setEmailOrUsername(String emailOrUsername) {
+        this.emailOrUsername = emailOrUsername;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
